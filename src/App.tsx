4d97c26b93eb2ad5a8103ae89c3585e49e4743cf
Sergey Arefkin <arefkins@globalbit.co.il>
2025-08-05 17:24:01 +0400

    breadcrumbs demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import logo from '/logo.png';
-import { Accordion, AccordionItem, Button } from '@igds/react';
+import { Button, Breadcrumbs, BreadcrumbItem } from '@igds/react';
 import './App.css'
 
 function App() {
@@ -12,19 +12,50 @@ function App() {
         </a>
       </div>
       <h1>IGDS packages test</h1>
-      <Accordion multiexpand className="accordion">
-          <AccordionItem
-            icon="help-outlined"
-            header="כותרת"
-          >
-            <p>
-              רכיב האקורדיון מאפשר הצגת תוכן במרחב קטן דרך חשיפה הדרגתית. המשתמש
-              מקבל פרטים מרכזיים על התוכן ויכול לבחור להרחיב את התוכן הזה על ידי
-              פתיחת הרכיב.
-            </p>
-          </AccordionItem>
-        </Accordion>
+      
       <Button>כפתור בדיקה</Button>
+
+      <section dir="rtl" style={{margin: '2rem 0 0'}}>
+        <Breadcrumbs label="Default Breadcrumbs">
+          <BreadcrumbItem href="/">
+            דף הבית
+          </BreadcrumbItem>
+          <BreadcrumbItem href="/services">
+            שירותים ומדורים
+          </BreadcrumbItem>
+          <BreadcrumbItem href="/services/health">
+            בריאות הציבור
+          </BreadcrumbItem>
+          <BreadcrumbItem href="/services/health/travel">
+            נסיעות לחו"ל
+          </BreadcrumbItem>
+          <BreadcrumbItem href="/services/health/travel/requirements">
+            דרישות בריאותיות
+          </BreadcrumbItem>
+          <BreadcrumbItem href="/services/health/travel/requirements/visa">
+            אישורי כניסה למדינות
+          </BreadcrumbItem>
+          <BreadcrumbItem href="/services/health/travel/requirements/visa/asia">
+            מדינות אסיה
+          </BreadcrumbItem>
+          <BreadcrumbItem href="/services/health/travel/requirements/visa/asia/thailand">
+            תאילנד
+          </BreadcrumbItem>
+          <BreadcrumbItem href="/services/health/travel/requirements/visa/asia/thailand/vaccination">
+            חיסונים נדרשים
+          </BreadcrumbItem>
+          <BreadcrumbItem href="/services/health/travel/requirements/visa/asia/thailand/vaccination/certificate">
+            תעודת חיסונים בינלאומית
+          </BreadcrumbItem>
+          <BreadcrumbItem href="/services/health/travel/requirements/visa/asia/thailand/vaccination/certificate/apply">
+            הגשת בקשה
+          </BreadcrumbItem>
+          <BreadcrumbItem href="/services/health/travel/requirements/visa/asia/thailand/vaccination/certificate/apply/status">
+            מעקב אחר בקשה
+          </BreadcrumbItem>
+        </Breadcrumbs>
+      </section>
+
     </section>
   )
 }
